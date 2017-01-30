0b2245df3c80ed4e42382c5ebf1ab217f6ba14e1
Galina Kochneva <checkbox88@gmail.com>
2017-01-29 23:39:36 -0800

    Вынесла методы из ApplicationManager в SessionHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,5 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -14,6 +13,7 @@ public class ApplicationManager {
 
   private FirefoxDriver wd;
 
+  private SessionHelper sessionHelper;
   private ContactHelper contactHelper;
   private GroupHelper groupHelper;
   private NavigationHelper navigationHelper;
@@ -34,17 +34,8 @@ public class ApplicationManager {
     groupHelper = new GroupHelper(wd);
     contactHelper = new ContactHelper(wd);
     navigationHelper = new NavigationHelper(wd);
-    login("admin", "secret");
-  }
-
-  private void login(String username, String password) {
-      wd.findElement(By.name("pass")).click();
-      wd.findElement(By.name("pass")).clear();
-      wd.findElement(By.name("pass")).sendKeys(password);
-      wd.findElement(By.name("user")).click();
-      wd.findElement(By.name("user")).clear();
-      wd.findElement(By.name("user")).sendKeys(username);
-      wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    sessionHelper = new SessionHelper(wd);
+    sessionHelper.login("admin", "secret");
   }
 
   public void gotoGroupPage() {

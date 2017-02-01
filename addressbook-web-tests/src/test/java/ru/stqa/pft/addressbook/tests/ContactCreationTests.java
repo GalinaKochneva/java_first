baa45c512ff90de1b08e416c1e49c2eee2782b37
Galina Kochneva <checkbox88@gmail.com>
2017-02-01 15:12:39 -0800

    Оптимизация заполнения форм: оставляем дефолтные значения в полях. null

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -8,7 +8,7 @@ public class ContactCreationTests extends TestBase {
     @Test
     public void ContactCreationTests() {
         app.goToAddNewPage();
-        app.getContactHelper().fillContactForm(new ContactData("Fuffa", "Namratova", "1111111111", "Naamratova@gmail.com"));
+        app.getContactHelper().fillContactForm(new ContactData("Fuffa", "Namratova", null, null));
         app.getContactHelper().inputContactCreation();
     }
 

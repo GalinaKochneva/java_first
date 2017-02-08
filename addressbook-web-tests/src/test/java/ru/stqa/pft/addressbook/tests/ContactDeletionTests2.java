d7d2ff583b86ad87660baa46a755e13326dab65e
Galina Kochneva <checkbox88@gmail.com>
2017-02-07 23:28:25 -0800

    Количество элементов в коллекции в Contacts

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests2.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests2.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -7,9 +8,12 @@ public class ContactDeletionTests2 extends TestBase {
 
     @Test
     public void testContactDeletion() {
+        int before = app.getContactHelper().getContactCount();
         if (! app.getContactHelper().isThereAContact()) {
             app.getContactHelper().createContact(new ContactData("Fuffa", "Namratova", null, null, "[none]"));
             app.goToHomePage();
+            int after = app.getContactHelper().getContactCount();
+            Assert.assertEquals(after, before + 1);
         }
         app.getContactHelper().openContactForEdit();
         app.getContactHelper().deleteContact();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -11,9 +12,12 @@ public class ContactModificationTests extends TestBase {
   @Test
   public void testContactModification() {
     app.goToHomePage();
+    int before = app.getContactHelper().getContactCount();
     if (! app.getContactHelper().isThereAContact()) {
       app.getContactHelper().createContact(new ContactData("Fuffa", "Namratova", null, null, "[none]"));
       app.goToHomePage();
+      int after = app.getContactHelper().getContactCount();
+      Assert.assertEquals(after, before + 1);
     }
     app.getContactHelper().selectContact();
     app.getContactHelper().openContactForEdit();

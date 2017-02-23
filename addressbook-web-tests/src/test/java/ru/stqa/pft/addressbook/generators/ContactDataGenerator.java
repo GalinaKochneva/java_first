64252eb396fb039fb8659dbdc38c89359e909a52
Galina Kochneva <checkbox88@gmail.com>
2017-02-22 18:00:14 -0800

    6v3Анализ опций командой строки при запуске генератора данных Contacts

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -1,6 +1,9 @@
 package ru.stqa.pft.addressbook.generators;
 
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.io.File;
@@ -12,16 +15,30 @@ import java.util.List;
 
 public class ContactDataGenerator {
 
+  @Parameter(names = "-c", description = "Contact count")
+  public int count;
+
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
+
   public static void main(String[] args) throws IOException {
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
-
-    List<ContactData> contacts = generateContacts(count);
-    save(contacts, file);
-
+    ContactDataGenerator generator = new ContactDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex) {
+      jCommander.usage();
+      return;
+    }
+    generator.run();
   }
 
-  private static void save(List<ContactData> contacts, File file) throws IOException {
+  private void run() throws IOException {
+    List<ContactData> contacts = generateContacts(count);
+    save(contacts, new File(file));
+  }
+
+  private void save(List<ContactData> contacts, File file) throws IOException {
     System.out.println(new File(".").getAbsolutePath());
     Writer writer = new FileWriter(file);
     for (ContactData contact : contacts) {
@@ -31,12 +48,12 @@ public class ContactDataGenerator {
     writer.close();
   }
 
-  private static List<ContactData> generateContacts(int count) {
+  private List<ContactData> generateContacts(int count) {
     List<ContactData> contacts = new ArrayList<ContactData>();
     for (int i = 0; i < count; i++) {
       contacts.add(new ContactData().withFirstname(String.format("Ryan %s", i))
               .withLastname(String.format("Tedder %s", i)).withAddress(String.format("Colorado, USA %s", i))
-              .withEmail(String.format("Ryan@gmail.com %s", i)).withAllPhones(String.format("777777777 \n 333333 \n 888888 %s", i)));
+              .withEmail(String.format("Ryan@gmail.com %s", i)).withAllPhones(String.format("777777777, 333333, 888888 %s", i)));
     }
     return contacts;
   }

732a1ef28871b144a72ec14aac323dc46d6fbd87
Galina Kochneva <checkbox88@gmail.com>
2017-02-22 23:03:32 -0800

    6v4Параметризация тестовых методов Contacts

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,10 +1,14 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -13,14 +17,26 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testContactCreation() {
-    Contacts before = app.contact().all();
+  @DataProvider
+  public Iterator<Object[]> validContacts() {
+    List<Object[]> list = new ArrayList<Object[]>();
     File photo = new File("src/test/resources/rose.jpg");
-    ContactData contact = new ContactData().withFirstname("Fuffochka").withLastname("Namratova")
-            .withAddress("1202 Emerald st., Mountain View, USA")
-            .withEmail("Naamratova@gmail.com").withHomePhone("+7(927)5673353").withMobilePhone("22-22-22")
-            .withWorkPhone("33 33 33").withGroup("[none]").withPhoto(photo);
+    list.add(new Object[] {new ContactData().withFirstname("Ryan").withLastname("Tedder").withAddress("Colorado, USA")
+            .withEmail("Ryan@gmail.com").withHomePhone("+7(927)5673353").withMobilePhone("22-22-22")
+            .withWorkPhone("33 33 33").withPhoto(photo).withGroup("[none]")});
+    list.add(new Object[] {new ContactData().withFirstname("Ryan 2").withLastname("Tedder 2").withAddress("Colorado, USA 2")
+            .withEmail("Ryan@gmail.com 2").withHomePhone("+7(927)5673353 2").withMobilePhone("22-22-22 2")
+            .withWorkPhone("33 33 33 3").withPhoto(photo)
+            .withGroup("[none]")});
+    list.add(new Object[] {new ContactData().withFirstname("Ryan 3").withLastname("Tedder 3")
+            .withAddress("Colorado, USA 3").withEmail("Ryan@gmail.com 3").withHomePhone("+7(927)5673353 3")
+            .withMobilePhone("22-22-22 3").withWorkPhone("33 33 33 3").withPhoto(photo).withGroup("[none]")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) {
+    Contacts before = app.contact().all();
     app.contact().create(contact);
     Contacts after = app.contact().all();
     assertThat(app.contact().count(), equalTo(before.size() + 1));

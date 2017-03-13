64909bae89541dbf90f2863367a8bfbeca14da51
Galina Kochneva <checkbox88@gmail.com>
2017-03-12 19:40:57 -0700

    8v6 Получение почты на встроенный в тесты почтовый сервер

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -10,3 +10,6 @@ $g_default_timezone       = 'Europe/Berlin';
 $g_crypto_master_salt     = 'peTJWapOqC1e8QIgebdLrE3JewmBf8m58WIjdDxlXAE=';
 
 $g_signup_use_captcha = OFF;
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
+$g_smtp_port = 12345;
